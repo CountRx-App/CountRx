fd7e54f27930cbff7a5221d0b62511d952e35c17
watsonet <watsonet@rose-hulman.edu>
2023-10-30 13:05:48 -0400

    Edit PillCount

--- count_rx/lib/models/pill_count.dart
+++ count_rx/lib/models/pill_count.dart
@@ -1,1 +1,14 @@
-class PillCount {}
+class PillCount {
+  int count = 0;
+  String name;
+  final DateTime timestamp = DateTime.now();
+
+  PillCount({
+    required this.count,
+    this.name = "",
+  });
+
+  set setCount(int count) {
+    this.count = count;
+  }
+}
